b875cb621d3b30b981764b8e396a8ceb4b5383e5
Jay Thurber <jthurber87@gmail.com>
2021-08-27 15:12:54 -0600

    router working, body parser included in server.js

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose');
 const db = mongoose.connection
-const Projects = require('./models/projects.js')
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
-
+const projectsController = require('./controllers/projects.js');
 
 //MONGOOSE CONNECT
 mongoose.connect('mongodb://localhost:27017/saltbae', { useNewUrlParser: true}, { useUnifiedTopology: true });
@@ -20,87 +19,8 @@ db.once('open', ()=> {
 app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }));
-
-//ROUTES
-
-// Index route
-app.get('/', (req, res) => {
-    res.redirect("/saltbae")
-})
-
-app.get('/saltbae', (req,res)=>{
-    Projects.find({}, (err, allProjects)=>{
-        if(err){
-            res.send(err)
-        }else {
-            res.render('index.ejs', {projects: allProjects})
-        }
-    })
-})
-
-// New route
-app.get('/saltbae/new', (req, res) => {
-  res.render('new.ejs')
-})
-
-// Post route
-app.post('/saltbae/:id',(req,res)=>{
-  Projects.create(req.body, (err, newProject)=>{
-    if (err) {
-      console.log(err);
-    } else {
-      res.redirect('/saltbae')
-    }
-  })
-})
-
-// Show route
-app.get('/saltbae/:id', (req, res)=> {
-  let id = req.params.id
-  Projects.findById(id, (err, foundProject)=>{
-    if(err){
-      res.send(err)
-    } else{
-      res.render('show.ejs', {project: foundProject, id: id})
-    }
-  })
-})
-
-
-// Delete route
-app.delete('/saltbae/:id', (req, res) => {
-    Projects.deleteOne({_id: req.params.id}, (err) => {
-      if (err) {
-        res.send(err)
-      } else {
-        res.redirect('/saltbae')
-      }
-    })
-})
-
-// Edit route
-app.get('/saltbae/:id/edit', (req, res)=>{
-  let id = req.params.id
-  Projects.findById(id, (err, foundProject)=>{
-    if (err) {
-      res.send(err)
-    } else {
-      res.render('edit.ejs', {project: foundProject})
-    }
-  })
-})
-
-// Put route
-app.put('/saltbae/:id', (req,res)=>{
-    Projects.findByIdAndUpdate(req.params.id, req.body, {new: true},(err)=>{
-        if(err){
-            res.send(err)
-        }else {
-            res.redirect('/saltbae')
-        }
-    })
-})
-
+app.use(bodyParser.urlencoded({extended: true}))
+app.use('/saltbae', projectsController);
 app.listen(port, () => {
     console.log("App listening on port " + port)
 })
